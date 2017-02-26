8eda2052450a315131ea49c46f03fc971d57c970
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:40:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'Antivirus foinse oscailte agus scanóir antimalware do freastalaithe ríomhphoist.';
+$lang['mail_antivirus_app_name'] = 'Mail Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'Mail Polasaithe';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Athraigh Socruithe Geata Antivirus';
