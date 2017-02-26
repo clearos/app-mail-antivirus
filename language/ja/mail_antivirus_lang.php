8ed432253f97167b3b51bb8e6d7bfec4b70aeda5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:40:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'メールサーバーのオープンソースのアンチウイルスおよびアンチマルウェアスキャナ。';
+$lang['mail_antivirus_app_name'] = 'メールアンチウイルス';
+$lang['mail_antivirus_mail_policies'] = 'メールポリシー';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'ゲートウェイアンチウィルスの設定を変更';
