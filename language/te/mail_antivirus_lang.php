40151602642acf6d0c75b1bec4c60008890d8037
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:46:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'ఒపెన్ సోర్స్ యాంటీవైరస్ మరియు మెయిల్ సర్వర్లు కోసం Antimalware స్కానర్.';
+$lang['mail_antivirus_app_name'] = 'మెయిల్ యాంటీవైరస్';
+$lang['mail_antivirus_mail_policies'] = 'మెయిల్ విధానాలు';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'గేట్వే యాంటీవైరస్ సెట్టింగులు సవరించు';
