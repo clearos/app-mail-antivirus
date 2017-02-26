dc275bc387a9dd81ab102d1f2d2c843e0e79b565
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:41:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'ການປ້ອງກັນໄວລັດເປີດເຜີຍ, ແຫຼ່ງແລະສະແກນເນີ antimalware ສໍາລັບການເຄື່ອງແມ່ຂ່າຍເມລ.';
+$lang['mail_antivirus_app_name'] = 'mail Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'ນະໂຍບາຍ mail';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'ປັບປຸງແກ້ໄຂການຕັ້ງຄ່າ Gateway Antivirus';
