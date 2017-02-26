6994baa4efcb82ea648eadae53f000e041a7f938
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:45:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'Un antivirus de código abierto y el escáner antimalware para servidores de correo.';
+$lang['mail_antivirus_app_name'] = 'Antivirus del correo';
+$lang['mail_antivirus_mail_policies'] = 'Directivas de correo';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Modificación de la configuración de puerta de enlace Antivirus';
