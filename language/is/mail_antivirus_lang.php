21e674d509276188e0e0690b1db04be17fd24a44
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:40:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'Opinn-uppspretta antivirus og antimalware skanni fyrir póstþjóna.';
+$lang['mail_antivirus_app_name'] = 'mail Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'Mail Reglur';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Breyta Gateway vírusvarnarstillingunum';
