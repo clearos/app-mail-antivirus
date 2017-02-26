a17313ba9f9a54e7018225e00739eee4b783844d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:45:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'Open-source antivirus in antimalware skener za poštne strežnike.';
+$lang['mail_antivirus_app_name'] = 'mail Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'mail Politike';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Spreminjanje Gateway Antivirus nastavitve';
