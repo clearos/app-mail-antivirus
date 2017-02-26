842aac3eda5d9d3c2f4429a8f53440bfc1551124
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:45:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'En öppen källkod antivirus och antimalware scanner för e-postservrar.';
+$lang['mail_antivirus_app_name'] = 'post Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'post Riktlinjer';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Modifiera Gateway Antivirus-inställningar';
