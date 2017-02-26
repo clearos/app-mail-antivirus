6a6588475642af21585d3ce7ed00acd03d36793e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:47:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'אַן אָפֿן-מקור אַנטיווירוס און אַנטימאַלוואַרע סקאַננער פֿאַר פּאָסט סערווערס.';
+$lang['mail_antivirus_app_name'] = 'מעיל אַנטיווירוס';
+$lang['mail_antivirus_mail_policies'] = 'מעיל פּאָליסיעס';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'מאָדיפיצירן Gateway אַנטיווירוס סעטטינגס';
