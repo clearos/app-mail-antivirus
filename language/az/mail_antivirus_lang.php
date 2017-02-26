f3ecf22ac2d2455a2aceac9d569b8238c8f1e06d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:36:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'poçt serverləri üçün açıq mənbə antivirus və antimalware skaner.';
+$lang['mail_antivirus_app_name'] = 'Mail Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'Mail siyasəti';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Gateway Antivirus parametrlərini dəyişmək';
