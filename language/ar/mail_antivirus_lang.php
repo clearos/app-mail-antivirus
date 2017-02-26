9ad321f256e14cccf913f2b9bdb09ea3a7d82d7f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:36:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'المصدر المفتوح برامج مكافحة الفيروسات ومكافحة البرامج الضارة الماسح الضوئي لخدمة البريد.';
+$lang['mail_antivirus_app_name'] = 'البريد مكافحة الفيروسات';
+$lang['mail_antivirus_mail_policies'] = 'سياسات الإلكتروني';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'تعديل إعدادات بوابة مكافحة الفيروسات';
