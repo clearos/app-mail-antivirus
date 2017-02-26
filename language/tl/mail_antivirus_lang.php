4d6ba3148a0a15da31b71c8d454611860ddaac05
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:46:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'Isang bukas-source antivirus at antimalware scanner para sa mail server.';
+$lang['mail_antivirus_app_name'] = 'mail Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'Mga Patakaran mail';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Baguhin Setting Gateway Antivirus';
