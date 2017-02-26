1036be8549e61ab267ebf02d216c42380b002876
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:43:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'Open-source antivirus u scanner antimalware għall mail servers.';
+$lang['mail_antivirus_app_name'] = 'posta Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'Politiki mail';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Tibdel Gateway Antivirus Settings';
