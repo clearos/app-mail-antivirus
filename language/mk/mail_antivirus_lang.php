496362e89a33fa7706d5ab9205183041d94943f5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:42:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'Антивирус софтвер со отворен код и Antimalware скенер за mail сервери.';
+$lang['mail_antivirus_app_name'] = 'пошта антивирус';
+$lang['mail_antivirus_mail_policies'] = 'политики пошта';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Менување на прилагодувањата Портал антивирус';
