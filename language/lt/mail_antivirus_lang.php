6ca53215a558a5559bbee12b44c6d64472d53d65
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:41:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'Atviro kodo antivirusinė ir Antimalware skaitytuvas pašto serverių.';
+$lang['mail_antivirus_app_name'] = 'paštas Antivirusinė';
+$lang['mail_antivirus_mail_policies'] = 'paštas politika';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Keisti Vartai Antivirus Nustatymai';
