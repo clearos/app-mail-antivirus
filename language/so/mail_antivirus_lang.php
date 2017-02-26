c45377bc3a1ea63520c84f75c23c6c33cbc9fb2b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:45:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'An antivirus-il furan iyo scanner antimalware for server mail.';
+$lang['mail_antivirus_app_name'] = 'Mail Adda';
+$lang['mail_antivirus_mail_policies'] = 'Siyaasadaha Mail';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Badalayo Settings Gateway Adda';
