399d811d90f848301b56ebed7bba1c6a769176a4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:41:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'ជាការកំចាត់មេរោគកម្មវិធីប្រភពបើកចំហនិងម៉ាស៊ីនស្កេន antimalware សម្រាប់ម៉ាស៊ីនបម្រើសំបុត្រ។';
+$lang['mail_antivirus_app_name'] = 'សំបុត្រប្រឆាំងមេរោគ';
+$lang['mail_antivirus_mail_policies'] = 'គោលនយោបាយសំបុត្រ';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'កែប្រែការកំណត់ប្រឆាំងមេរោគផ្លូវចេញចូល';
