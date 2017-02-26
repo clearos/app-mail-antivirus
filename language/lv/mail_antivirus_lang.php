86f1fcf15229d24ecb5cc54d63937e114af85d5c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:42:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'Atvērtā koda antivīrusa un antimalware skeneris pasta serveriem.';
+$lang['mail_antivirus_app_name'] = 'pasts Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'pasta politikas';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Modificēt Gateway Antivirus Settings';
