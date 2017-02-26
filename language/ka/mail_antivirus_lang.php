9dc93d3fc682408a257fabc7d6b5c7ff62b2ec7a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:41:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'ღია ანტივირუსული და antimalware სკანერი ფოსტა სერვერები.';
+$lang['mail_antivirus_app_name'] = 'ფოსტა Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'mail პოლიტიკა';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'ცვლილებები Gateway Antivirus პარამეტრები';
