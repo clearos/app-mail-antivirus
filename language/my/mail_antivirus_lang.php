702a508825cd4ffae1d9a5a014d8deeab95b6429
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:37:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'မေးလ်ဆာဗာများအတွက် open-source ကို antivirus ကိုနှင့် antimalware ကင်နာ။';
+$lang['mail_antivirus_app_name'] = 'မေးလ် Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'မေးလ်မူဝါဒများ';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Gateway မှာ Antivirus ကိုချိန်ညှိပြုပြင်မွမ်းမံ';
