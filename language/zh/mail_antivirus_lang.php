9b88a3966a965121fcea2b55071af52e81a99403
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:37:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = '一个开源的防病毒和反恶意软件扫描邮件服务器。';
+$lang['mail_antivirus_app_name'] = '邮件病毒';
+$lang['mail_antivirus_mail_policies'] = '邮件策略';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = '修改网关防病毒设置';
