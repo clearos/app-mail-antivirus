74aaa6699574a62f8dbb5201f59064b97875232b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:46:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'posta sunucuları için bir açık kaynak kodlu bir antivirüs ve antimalware tarayıcı.';
+$lang['mail_antivirus_app_name'] = 'posta Antivirüs';
+$lang['mail_antivirus_mail_policies'] = 'posta Politikaları';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Ağ Geçidi Antivirüs Ayarlarını Değiştir';
