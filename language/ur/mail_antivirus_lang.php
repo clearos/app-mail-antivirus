932222ac434e8f9b6f022e6ab7f5eb4068095ba2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:47:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'میل سرورز کے لئے ایک اوپن سورس ینٹیوائرس اور لئے Antimalware سکینر.';
+$lang['mail_antivirus_app_name'] = 'میل ینٹیوائرس';
+$lang['mail_antivirus_mail_policies'] = 'میل پالیسیاں';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'گیٹ وے اینٹی وائرس کی ترتیبات میں ترمیم';
