efb83b423dbe941c6d07f33c05b878e5284898b1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:42:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'He wheori tuwhera-puna, me te matawai tārewa mō tūmau mēra.';
+$lang['mail_antivirus_app_name'] = 'Mail pareketo';
+$lang['mail_antivirus_mail_policies'] = 'kaupapa here mēra';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Whakakē Tautuhinga Gateway pareketo';
