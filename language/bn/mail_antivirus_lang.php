5496c12619847a4a887e74ee8f6669bb69f14f2b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:37:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'একটি ওপেন সোর্স অ্যান্টিভাইরাস এবং মেইল ​​সার্ভারের জন্য অ্যান্টিম্যালওয়্যার স্ক্যানারের মাধ্যমে এটি.';
+$lang['mail_antivirus_app_name'] = 'মেল অ্যান্টিভাইরাস';
+$lang['mail_antivirus_mail_policies'] = 'মেল নীতিসমূহ';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'গেটওয়ে অ্যান্টিভাইরাস সেটিংস পরিবর্তন করুন';
