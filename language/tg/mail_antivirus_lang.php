34001264a7b6a9a8aef3abd1e8b5ef84b89e1138
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:46:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'Як Антивирус сарчашмаашон кушоди ва сканер antimalware барои коргузорони почтаи.';
+$lang['mail_antivirus_app_name'] = 'Mail Антивирусҳо';
+$lang['mail_antivirus_mail_policies'] = 'сиёсати Mail';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Тағир Танзимотҳо Gateway Антивирусҳо';
