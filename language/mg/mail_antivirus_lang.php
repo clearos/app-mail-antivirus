925d56ee27f64e6999ff04f51a3c50a3f7ff68ab
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:42:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'Ny open-source sy ny antimalware scanner Antivirus ho namany Sary lohamilina.';
+$lang['mail_antivirus_app_name'] = 'Mail Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'Mail Policies';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Manova Gateway Antivirus Settings';
