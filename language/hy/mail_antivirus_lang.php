4e9afdaa4ba48510ca5de04241f3146a69d43e16
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:36:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'Բաց կոդով հակավիրուսային եւ antimalware սկաների համար փոստի սերվերների.';
+$lang['mail_antivirus_app_name'] = 'Փոստ Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'Փոստ քաղաքականություն';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Փոփոխել Gateway հակավիրուսային կայանքները';
