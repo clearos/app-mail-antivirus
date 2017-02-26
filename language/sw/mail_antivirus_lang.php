b7718e35f90a1dd241826b2f82b6b92d8db3eb58
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:45:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'antivirus wazi chanzo na antimalware Scanner kwa servrar pepe.';
+$lang['mail_antivirus_app_name'] = 'Barua Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'Sera za Barua';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Kurekebisha Gateway Antivirus Settings';
