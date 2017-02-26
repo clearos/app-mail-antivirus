ef0fcfe03666bd21a580a4b218fa0c1e047fda76
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:36:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = '&#39;N open-source antivirus en AntiMalware skandeerder vir posbedieners.';
+$lang['mail_antivirus_app_name'] = 'Mail Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'Mail beleid';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Verander Gateway Antivirus-instellings';
