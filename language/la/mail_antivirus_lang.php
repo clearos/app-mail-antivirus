2068c5cb1a5de236e32ddf6db6d2c86cf3e8ed80
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:41:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'An open source Antivirus-mail et antimalware Scanner enim servientibus.';
+$lang['mail_antivirus_app_name'] = 'Mail Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'Mail Politiae';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Temperare Antivirus Occasus Gateway';
