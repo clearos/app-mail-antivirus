4f99bee81aa819f467aa69a8483c01ef90ba01ec
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:47:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'Một virus mã nguồn mở và quét malware cho các máy chủ mail.';
+$lang['mail_antivirus_app_name'] = 'thư Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'Chính sách thư';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Sửa đổi Cài đặt Gateway Antivirus';
