353e2e6be8c16b5b21bb5c42fcabf6344606dddd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:40:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'Na-emeghe-isi iyi antivirus na antimalware nyocha maka mail sava.';
+$lang['mail_antivirus_app_name'] = 'mail Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'mail atumatu';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Gbanwee Gateway Antivirus Ntọala';
