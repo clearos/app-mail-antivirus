233df9e61615ba6b705128f340987b75c15ab340
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:37:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'An antivayirasi lofuna gwero ndi antimalware sikana kwa maseva makalata.';
+$lang['mail_antivirus_app_name'] = 'Mail antivayirasi';
+$lang['mail_antivirus_mail_policies'] = 'Ndondomeko Mail';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Kusintha Polakwika antivayirasi Zikhazikiko';
