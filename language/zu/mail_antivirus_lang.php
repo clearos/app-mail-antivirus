bbb6ae7a0c2f4a5eb7df6a1dfca5d5c8718fbd3e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:48:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'An unqulo wesi arab levulekile yomthombo kanye isithwebuli antimalware imeyili amaseva.';
+$lang['mail_antivirus_app_name'] = 'Mail Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'Izinqubomgomo Mail';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Guqula izilungiselelo Gateway wesi arab kuphela';
