2fecb90b0060a7154abf314ba2363515e2d5f599
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:47:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'Mae antivirus ffynhonnell agored a sganiwr Antimalware gyfer gweinyddwyr bost.';
+$lang['mail_antivirus_app_name'] = 'Mail Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'Polisïau Mail';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Addasu Gosodiadau Porth Antivirus';
