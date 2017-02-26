26313230cde41a2671da7316b3ec710d945ba6c9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:41:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'пошта сервер үшін ашық көзі вирусқа қарсы және вирусқа қарсы сканер.';
+$lang['mail_antivirus_app_name'] = 'Mail Антивирус';
+$lang['mail_antivirus_mail_policies'] = 'Пошта саясаты';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Шлюз Антивирус параметрлерін өзгерту';
