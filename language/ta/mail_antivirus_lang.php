dad533156e5467db12980a1bdf0d2a2ff61bcb3f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:46:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'மெயில் சர்வர்கள் ஒரு திறந்த மூல வைரஸ் தடுப்பு மற்றும் Antimalware ஸ்கேனர்.';
+$lang['mail_antivirus_app_name'] = 'மின்னஞ்சல் வைரஸ்';
+$lang['mail_antivirus_mail_policies'] = 'மின்னஞ்சல் கொள்கைகள்';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'நுழைவாயில் வைரஸ் அமைப்புகளை மாற்ற';
