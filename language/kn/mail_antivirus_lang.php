4e6ad9366af9c99ef55276e6a5342a5992042c14
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:41:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'ಒಂದು ತೆರೆದ ಮೂಲ ಆಂಟಿವೈರಸ್ ಮತ್ತು ಮೇಲ್ ಸರ್ವರ್ಗಳು Antimalware ಸ್ಕ್ಯಾನರ್.';
+$lang['mail_antivirus_app_name'] = 'ಮೇಲ್ ಆಂಟಿವೈರಸ್';
+$lang['mail_antivirus_mail_policies'] = 'ಮೇಲ್ ನೀತಿಗಳು';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'ಗೇಟ್ವೇ ಆಂಟಿವೈರಸ್ ಸೆಟ್ಟಿಂಗ್ಗಳನ್ನು ಮಾರ್ಪಡಿಸಿ';
