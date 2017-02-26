0036ecac9029f3879c880f9bbf30fbcc3e14427c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:45:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'An antiviru bulehileng mohloli le antimalware itlho bakeng server mail.';
+$lang['mail_antivirus_app_name'] = 'Mail antiviru';
+$lang['mail_antivirus_mail_policies'] = 'Maano Mail';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Fetoloa tsela ea heke antiviru Settings';
