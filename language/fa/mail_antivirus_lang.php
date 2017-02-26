c53e0d512567cc4c94417a06d3f0a9aa2370ad28
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:43:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'یک آنتی ویروس متن باز و اسکنر ضد بدافزار برای سرویس دهنده پست الکترونیکی.';
+$lang['mail_antivirus_app_name'] = 'ایمیل آنتی ویروس';
+$lang['mail_antivirus_mail_policies'] = 'سیاست ایمیل';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'تغییر تنظیمات دروازه آنتی ویروس';
