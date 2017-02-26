e865f7ffcfb48ee05103a61a222fab40f5cc457a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:46:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'โปรแกรมป้องกันไวรัสที่มาเปิดและสแกนเนอร์มัลแวร์สำหรับเซิร์ฟเวอร์อีเมล';
+$lang['mail_antivirus_app_name'] = 'จดหมายป้องกันไวรัส';
+$lang['mail_antivirus_mail_policies'] = 'นโยบายจดหมาย';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'แก้ไขการตั้งค่าเกตเวย์ป้องกันไวรัส';
