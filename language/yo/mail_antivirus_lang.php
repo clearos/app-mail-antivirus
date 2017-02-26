bddd4bdd4fb2174a9579948b177b0b0b4e362db8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:47:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'Ohun-ìmọ-orisun antivirus ki o si antimalware scanner fun mail apèsè.';
+$lang['mail_antivirus_app_name'] = 'mail Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'mail imulo';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Yipada Gateway Antivirus Eto';
