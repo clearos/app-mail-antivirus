976f31a3fe056ebb1e510da5df95da3298d30281
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:42:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'мэйл серверүүд нь нээлттэй эх вирусны эсрэг болон antimalware нь скан хийгчийн.';
+$lang['mail_antivirus_app_name'] = 'Захидал Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'Захидал бодлого';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Gateway Antivirus Тохиргоо өөрчлөх';
