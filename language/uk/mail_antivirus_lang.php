2be61504165f7c60f6b0f5ceb564b8021ac189b6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:47:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'З відкритим вихідним кодом від вірусів і шкідливих програм сканер для поштових серверів.';
+$lang['mail_antivirus_app_name'] = 'Поштовий антивірус';
+$lang['mail_antivirus_mail_policies'] = 'політики поштових';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Зміна налаштувань шлюзу захисту від вірусів';
