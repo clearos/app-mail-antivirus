13ca40ca84aa6b99ce71787537ac1fbf38c5a74f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:44:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'Antywirusowe open-source i antimalware skaner dla serwerów pocztowych.';
+$lang['mail_antivirus_app_name'] = 'Poczta Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'Polityk Poczta';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Modyfikowanie ustawień bramy antywirusowe';
