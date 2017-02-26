7c210b927f3e84ba286eaba6c6fa7056b9570e57
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:39:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'Ένα antivirus ανοικτού κώδικα και antimalware σαρωτή για διακομιστές ηλεκτρονικού ταχυδρομείου.';
+$lang['mail_antivirus_app_name'] = 'Antivirus Mail';
+$lang['mail_antivirus_mail_policies'] = 'Πολιτικές Mail';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Τροποποίηση ρυθμίσεων Πύλη Antivirus';
