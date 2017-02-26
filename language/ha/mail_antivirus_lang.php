c811d2ae5e434fd0c17c63cc93a15af073480d4e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:39:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/mail_antivirus_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['mail_antivirus_app_description'] = 'An bude-source riga-kafi da kuma antimalware na&#39;urar daukar hotan takardu don mail sabobin.';
+$lang['mail_antivirus_app_name'] = 'Mail Antivirus';
+$lang['mail_antivirus_mail_policies'] = 'Mail Asiri';
+$lang['mail_antivirus_modify_gateway_antivirus_settings'] = 'Gyara Gateway Antivirus Saituna';
